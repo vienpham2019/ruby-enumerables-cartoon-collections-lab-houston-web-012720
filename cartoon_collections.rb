357794ea13bc37ed812d1283f548875804bad338
vien pham <vienpham2019@gmail.com>
2019-12-13 22:37:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index{|element,index| puts "#{index + 1}. #{element}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map{|element| "#{element.upercase}!"}
 end
 
 def long_planeteer_calls# code an argument here
