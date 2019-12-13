698b5496f89cb7130a6019745c7ade298da50e79
vien pham <vienpham2019@gmail.com>
2019-12-13 22:44:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map{|element| "#{element.upcase}!"}
+  array.map{|element| "#{element.capitalize}!"}
 end
 
 def long_planeteer_calls(array)# code an argument here
