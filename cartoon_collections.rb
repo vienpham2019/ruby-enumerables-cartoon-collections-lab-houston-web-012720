bafb9004db39f4554a7ffd8547025b3191f7cec6
vien pham <vienpham2019@gmail.com>
2019-12-13 22:42:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(array)# code an argument here
   array.map{|element| "#{element.upcase}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.collect{|element| break if element.length > 4}
+  return false
 end
 
 def find_the_cheese# code an argument here
