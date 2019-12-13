83e85e0379281a9e9045ae3ffb7515dbd17a47dd
vien pham <vienpham2019@gmail.com>
2019-12-13 22:44:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.collect{|element| break if element.length > 4}
+  array.collect{|element| break element if element.length > 4}
   return false
 end
 
