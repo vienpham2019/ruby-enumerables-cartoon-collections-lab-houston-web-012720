60c35434fff2c97c29190e43be939ad44d656b8a
vien pham <vienpham2019@gmail.com>
2019-12-13 22:58:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each_with_index{|element, index| return element if element.include?(cheese_types[index])}
+  array.each 
 end
