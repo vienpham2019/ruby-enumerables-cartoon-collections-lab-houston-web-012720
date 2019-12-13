60528e412ac4c2c0d44dd5f15847020249ad7d05
vien pham <vienpham2019@gmail.com>
2019-12-13 23:03:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each 
+  array.find {|element| 
 end
