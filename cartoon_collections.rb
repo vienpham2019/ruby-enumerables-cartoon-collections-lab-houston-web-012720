9d3a6eddb0f00a2ee9d76072a2efe21a5dc78b32
vien pham <vienpham2019@gmail.com>
2019-12-13 22:51:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def long_planeteer_calls(array)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.each_with_index{|element, index| element.include?(cheese_types[index])}
 end
